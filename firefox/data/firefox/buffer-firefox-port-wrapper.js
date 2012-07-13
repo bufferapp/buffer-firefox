beace1ff68e4d5a604523815068321b95aa70fe2
Tom Ashworth <tom@phuu.net>
2012-07-13 15:39:43 +0300

    Set options to defaults

--- firefox/data/firefox/buffer-firefox-port-wrapper.js
+++ firefox/data/firefox/buffer-firefox-port-wrapper.js
@@ -1,4 +1,4 @@
 if( !xt ) var xt = {};
 xt.port = self.port;
 
-xt.options = self.options;
+xt.options = {"buffer.op":"true","buffer.op.facebook":"facebook","buffer.op.hacker":"true","buffer.op.key-combo":"alt+b","buffer.op.key-enable":"key-enable","buffer.op.reader":"reader","buffer.op.reddit":"reddit","buffer.op.twitter":"twitter","buffer.run":"true"};
