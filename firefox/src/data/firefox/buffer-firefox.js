9d336edfbdeba97c360df83285837698ae1860fe
Philippe Masset <pioul06+github@gmail.com>
2016-07-19 17:04:14 +0200

    Tailor action when clicking toolbar button on single tweet page

--- firefox/src/data/firefox/buffer-firefox.js
+++ firefox/src/data/firefox/buffer-firefox.js
@@ -1,8 +1,27 @@
 /* globals self, bufferpm, bufferData */
 // Buffer for Firefox
 ;(function() {
+  // In most cases we'll want to simply open the overlay when a Buffer button
+  // is clicked. In some specific cases, we'll want to provide a more tailored
+  // experience, e.g. retweeting a tweet when we're on a tweet's permalink page
+  // and the Buffer toolbar button is clicked
   self.port.on("buffer_click", function(postData) {
-    bufferData(self.port, postData);
+    var isTweetPermalinkPage = /twitter\.com\/[^/]+\/status\/\d+/.test(window.location.href);
+    var $retweetButton = $('.permalink-tweet .ProfileTweet-action--buffer');
+
+    var shouldTriggerRetweetButtonClick = (
+      isTweetPermalinkPage &&
+      postData.placement === 'toolbar' &&
+      $retweetButton.length > 0
+    );
+
+    if (shouldTriggerRetweetButtonClick) {
+      $retweetButton.click();
+    } else {
+      // bufferData is a method in buffer-overlay that creates
+      // the overlay and gives it data.
+      bufferData(self.port, postData);
+    }
   });
 
   self.port.on("buffer_message_bind", function(postData) {
